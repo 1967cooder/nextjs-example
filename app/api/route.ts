347b8f8c90829d66ead0d7a0c78143cbf854756b
1967cooder <silvanalindholm@hotmail.com>
2026-03-27 14:59:31 +0200

    move fetch logic inside GET function and add env variable checks

--- app/api/route.ts
+++ app/api/route.ts
@@ -8,17 +8,25 @@
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
 
-// Direct API call to supabase using fetch
-const response = await fetch(`${supabaseUrl}/rest/v1/products`, {
-  headers: {
-    Authorization: `Bearer ${supabaseAnonKey}`,
-    apikey: supabaseAnonKey,
-    "Content-Type": "application/json",
-  },
-});
-
-const data = await response.json();
-
 export async function GET() {
+  if (!supabaseUrl || !supabaseAnonKey) {
+    return new Response(
+      JSON.stringify({ error: "Supabase credentials are missing" }),
+      {
+        status: 500,
+        headers: { "Content-Type": "application/json" },
+      },
+    );
+  }
+
+  const response = await fetch(`${supabaseUrl}/rest/v1/products`, {
+    headers: {
+      Authorization: `Bearer ${supabaseAnonKey}`,
+      apikey: supabaseAnonKey || "",
+      "Content-Type": "application/json",
+    },
+  });
+
+  const data = await response.json();
   return Response.json(data);
 }
